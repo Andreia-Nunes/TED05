639e3a535eaf8b2834896a861f45d474d1dec8bb
Andréia Nunes <andreia.nunes@academico.ifpb.edu.br>
2022-09-10 14:15:25 -0300

    first commit

---
+++ src/TED05.java
@@ -0,0 +1,18 @@
+//Aluna: Andréia Nunes Pereira
+
+public class TED05 {
+    public static void main(String[] args) {
+
+    int cont = 1;
+
+    while (cont <= 20){
+        System.out.printf("%d ", cont);
+
+        if (cont % 5 == 0){
+            System.out.println();
+        }
+
+        cont++;
+    }
+    }
+}
